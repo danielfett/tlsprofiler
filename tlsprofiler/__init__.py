9334b686dbe86d7ab29a504dfd90b3bd20ff2b7a
Fabian Hauck <hauckfabian@gmail.com>
2020-01-06 22:04:18 +0100

    check for hsts age

--- tlsprofiler/__init__.py
+++ tlsprofiler/__init__.py
@@ -1,6 +1,7 @@
 from sslyze.server_connectivity_tester import ServerConnectivityTester, ServerConnectivityError
 from sslyze.plugins.openssl_cipher_suites_plugin import *
 from sslyze.plugins.certificate_info_plugin import CertificateInfoScanCommand
+from sslyze.plugins.http_headers_plugin import HttpHeadersScanCommand, HttpHeadersScanResult
 from sslyze.synchronous_scanner import SynchronousScanner
 from sslyze.plugins.robot_plugin import RobotScanResultEnum, RobotScanCommand
 from sslyze.plugins.heartbleed_plugin import HeartbleedScanCommand
@@ -23,13 +24,17 @@ class TLSProfilerResult:
         self.vulnerable = len(self.vulnerability_errors) > 0
 
         self.all_ok = self.validated and self.profile_matched and not self.vulnerable
-        
+
+    def __str__(self):
+        return f"Validation Errors: {self.validation_errors}\n\nProfile Errors: {self.profile_errors}\n\n" \
+               f"Vulnerability Errors: {self.vulnerability_errors}\n\nValidated: {self.validated}\n\n" \
+               f"Profile Matched: {self.profile_matched}\n\nVulnerable: {self.vulnerable}\n\nAll ok: {self.all_ok}"
 
 
 class TLSProfiler:
     PROFILES_URL = 'https://statics.tls.security.mozilla.org/server-side-tls-conf-5.0.json'
     PROFILES = None
-    
+
     SCAN_COMMANDS = {
         "SSLv2": Sslv20ScanCommand,
         "SSLv3": Sslv30ScanCommand,
@@ -38,14 +43,15 @@ class TLSProfiler:
         "TLSv1.2": Tlsv12ScanCommand,
         "TLSv1.3": Tlsv13ScanCommand,
     }
-    
+
     def __init__(self, domain, target_profile_name):
         if TLSProfiler.PROFILES is None:
             TLSProfiler.PROFILES = requests.get(self.PROFILES_URL).json()
-            log.info(f"Loaded version {TLSProfiler.PROFILES['version']} of the Mozilla TLS configuration recommendations.")
+            log.info(
+                f"Loaded version {TLSProfiler.PROFILES['version']} of the Mozilla TLS configuration recommendations.")
 
         self.target_profile = TLSProfiler.PROFILES['configurations'][target_profile_name]
-            
+
         self.scanner = SynchronousScanner()
         try:
             server_tester = ServerConnectivityTester(
@@ -60,33 +66,31 @@ class TLSProfiler:
             self.server_error = e.error_message
             self.server_info = None
 
-        
     def run(self):
         if self.server_info is None:
             return
-        
+
         certificate_validation_errors = self.check_certificate()
+        hsts_errors = self.check_hsts_age()
         self.scan_supported_ciphers_and_protocols()
         profile_errors = self.check_server_matches_profile()
         vulnerability_errors = self.check_vulnerabilities()
 
         return TLSProfilerResult(
             certificate_validation_errors,
-            profile_errors,
+            profile_errors + hsts_errors,
             vulnerability_errors,
         )
 
-    
     def scan(self, command):
-        return self.scanner.run_scan_command(self.server_info, command())
-        
-        
+        return self.scanner.run_scan_command(self.server_info, command)
+
     def scan_supported_ciphers_and_protocols(self):
         supported_ciphers = []
         supported_protocols = []
         for name, command in self.SCAN_COMMANDS.items():
             log.debug(f"Testing protocol {name}")
-            result = self.scan(command)
+            result = self.scan(command())
             ciphers = [cipher.openssl_name for cipher in result.accepted_cipher_list]
             supported_ciphers.extend(ciphers)
             if len(ciphers):
@@ -94,10 +98,10 @@ class TLSProfiler:
 
         self.supported_ciphers = set(supported_ciphers)
         self.supported_protocols = set(supported_protocols)
-   
+
     def check_server_matches_profile(self):
         errors = []
-        
+
         allowed_protocols = set(self.target_profile['tls_versions'])
         illegal_protocols = self.supported_protocols - allowed_protocols
 
@@ -109,11 +113,24 @@ class TLSProfiler:
 
         for cipher in illegal_ciphers:
             errors.append(f'must not support "{cipher}"')
-     
+
         return errors
-    
+
+    def check_hsts_age(self) -> List[str]:
+        result = self.scan(HttpHeadersScanCommand())  # type: HttpHeadersScanResult
+
+        errors = []
+
+        if result.strict_transport_security_header:
+            if result.strict_transport_security_header.max_age < self.target_profile['hsts_min_age']:
+                errors.append(f"wrong HSTS age {result.strict_transport_security_header.max_age}")
+        else:
+            errors.append(f"HSTS header not set")
+
+        return errors
+
     def check_certificate(self):
-        result = self.scan(CertificateInfoScanCommand)
+        result = self.scan(CertificateInfoScanCommand())
 
         errors = []
 
@@ -138,7 +155,8 @@ class TLSProfiler:
             errors.append(f'Symantec legacy certificate found in chain.')
 
         if result.leaf_certificate_signed_certificate_timestamps_count < 2:
-            errors.append(f'Not enought SCTs in certificate, only found {result.leaf_certificate_signed_certificate_timestamps_count}.')
+            errors.append(
+                f'Not enought SCTs in certificate, only found {result.leaf_certificate_signed_certificate_timestamps_count}.')
 
         if len(errors) == 0:
             log.debug(f"Certificate is ok")
@@ -149,28 +167,31 @@ class TLSProfiler:
 
         return errors
 
-
     def check_vulnerabilities(self):
         errors = []
 
-        result = self.scan(HeartbleedScanCommand)
+        result = self.scan(HeartbleedScanCommand())
 
         if result.is_vulnerable_to_heartbleed:
             errors.append(f'Server is vulnerable to Heartbleed attack')
 
-        result = self.scan(OpenSslCcsInjectionScanCommand)
+        result = self.scan(OpenSslCcsInjectionScanCommand())
 
         if result.is_vulnerable_to_ccs_injection:
             errors.append(f'Server is vulnerable to OpenSSL CCS Injection (CVE-2014-0224)')
 
-        result = self.scan(RobotScanCommand)
+        result = self.scan(RobotScanCommand())
 
         if result.robot_result_enum in [
-                RobotScanResultEnum.VULNERABLE_WEAK_ORACLE,
-                RobotScanResultEnum.VULNERABLE_STRONG_ORACLE,
+            RobotScanResultEnum.VULNERABLE_WEAK_ORACLE,
+            RobotScanResultEnum.VULNERABLE_STRONG_ORACLE,
         ]:
             errors.append(f"Server is vulnerable to ROBOT attack.")
 
         return errors
 
 
+if __name__ == "__main__":
+    ca_file = "../../tlsprofiler_test/tests/certificates/rsa_ca_cert.pem"
+    profiler = TLSProfiler('old.dev.intranet', 'old')
+    print(profiler.run())
